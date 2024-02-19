a1f0d9bfef42314249e4f6f13e01cabeaf434416
unknown <anukrati19052002@gmail.com>
2024-02-19 22:35:52 +0530

    commit

---
+++ lib/screens/auth/login_screen.dart
@@ -0,0 +1,146 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:we_chat/screens/auth/phone.dart';
+
+import '../../api/apis.dart';
+import '../../helper/dialogs.dart';
+import '../../main.dart';
+import '../home_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _isAnimate = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    //for auto triggering animation
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() => _isAnimate = true);
+    });
+  }
+
+  _handleGoogleBtnClick() {
+    //for showing progress bar
+    Dialogs.showProgressBar(context);
+
+    _signInWithGoogle().then((user) async {
+      //for hiding progress bar
+      Navigator.pop(context);
+
+      if (user != null) {
+        log('\nUser: ${user.user}');
+        log('\nUserAdditionalInfo: ${user.additionalUserInfo}');
+
+        if ((await APIs.userExists())) {
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (_) => const HomeScreen()));
+        } else {
+          await APIs.createUser().then((value) {
+            Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (_) => const HomeScreen()));
+          });
+        }
+      }
+    });
+  }
+
+  Future<UserCredential?> _signInWithGoogle() async {
+    try {
+      await InternetAddress.lookup('google.com');
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth =
+      await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // Once signed in, return the UserCredential
+      return await APIs.auth.signInWithCredential(credential);
+    } catch (e) {
+      log('\n_signInWithGoogle: $e');
+      Dialogs.showSnackbar(context, 'Something Went Wrong (Check Internet!)');
+      return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Welcome to We Chat'),
+      ),
+      body: Stack(
+        children: [
+          AnimatedPositioned(
+            top: mq.height * .15,
+            right: _isAnimate ? mq.width * .25 : -mq.width * .5,
+            width: mq.width * .5,
+            duration: const Duration(seconds: 1),
+            child: Image.asset('images/icon.png'),
+          ),
+          Positioned(
+            bottom: mq.height * .15,
+            left: mq.width * .05,
+            width: mq.width * .9,
+            height: mq.height * .06,
+            child: ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 223, 255, 187),
+                shape: const StadiumBorder(),
+                elevation: 1,
+              ),
+              onPressed: _handleGoogleBtnClick,
+              icon: Image.asset('images/google.png', height: mq.height * .03),
+              label: RichText(
+                text: const TextSpan(
+                  style: TextStyle(color: Colors.black, fontSize: 16),
+                  children: [
+                    TextSpan(text: 'Login with '),
+                    TextSpan(
+                      text: 'Google',
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: mq.height * .05,
+            left: mq.width * .05,
+            width: mq.width * .9,
+            height: mq.height * .06,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => MyPhone()),
+                );
+              },
+              child: Text('Login with Phone Number'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
